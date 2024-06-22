6539ddaf1f7a159a3d3d62738b5fa8547f9bffcf
ann7he <ann7he@gmail.com>
2024-06-22 18:17:56 +0800

    GitHub Pages部署

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,40 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server:{
-    proxy:{
-      '/api':{//获取了路径中包含了api的请求
-        target:'http://47.120.31.153:8085',//后台服务所在的源
-        changeOrigin:true,//修改源
-        rewrite:(path)=>path.replace(/^\/api/,'')//api替换为空字符串
-      }
-    }
-  }
+    plugins: [
+        vue(),
+    ],
+    build: {
+        chunkSizeWarningLimit: 1000, // 调整警告限制为 1000 kB
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        proxy: {
+            '/api': {//获取了路径中包含了api的请求
+                target: 'http://47.120.31.153:8085',//后台服务所在的源
+                changeOrigin: true,//修改源
+                rewrite: (path) => path.replace(/^\/api/, '')//api替换为空字符串
+            }
+        }
+    },
+    base: '/fdzc-vue/' // 在生产环境中设置公共路径
 })
+
+
